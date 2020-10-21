7b0e87affd8fdd00ec38f1fff189cb493955083c
SavioChermont <saviochwt@gmail.com>
2020-10-21 15:30:09 -0300

    Busca imagens na Unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar.js'
 
 
 class App extends React.Component {
+
+    state = { images: [] }
     
-    onSearchSubmit(term){
-        console.log(term)
+    onSearchSubmit = async (term) => {
+        const result = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID ZZFsERqQXg6s9qllUFaakWbnx-n7st3jQ0JSK_9U440'
+            }
+        })
+
+        this.setState({images: result.data.results})
     }
 
     render(){
         return ( 
             <div className="ui container">
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found  {this.state.images.length}
             </div> );
     }
     
