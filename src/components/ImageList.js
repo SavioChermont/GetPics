592fb9cd95afb6893e00351281d234b8f1225baa
SavioChermont <saviochwt@gmail.com>
2020-10-21 17:48:47 -0300

    Adiciona GridCSS ao GetPics V2

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../css/ImageList.css';
+import ImageCard from './ImageCard.js';
 
 const ImageList = (props) => {
 
-    const arrayImg = props.images.map(({description, id, urls}) => {
-        return <img src={urls.regular} alt={description} key={id} />
+    const arrayImg = props.images.map((image) => {
+        return <ImageCard image={image} key={image.id}/>
     });
 
     return <div className="image-list">{arrayImg}</div>;
